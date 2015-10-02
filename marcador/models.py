2bf44a68db07c8e584276e1f7e3e5be8989a0e65
BillChabzz <chaby9.8@gmail.com>
2015-10-02 14:18:31 +0300

    added files

---
+++ marcador/models.py
@@ -0,0 +1,37 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from django.utils.timezone import now
+
+# Create your models here.
+@python_2_unicode_compatible
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    
+    class Meta:
+        verbose_name = 'tag'
+        verbose_name_plural = 'tags'
+        ordering = ['name']
+        
+    def __str__(self):
+        return seld.name
+
+@python_2_unicode_compatible    
+class Bookmark(models.Model):
+    url = models.URLField()
+    title = models.CharField('title', max_length=255)
+    description = models.TextField('description', blank=True)
+    is_public = models.BooleanField('public', default = True)
+    date_created = models.DateTimeField('date created')
+    date_updated = models.DateTimeField('date updated')
+    owner = models.ForeignKey(User, verbose_name='owner',
+                             related_name'bookmarks')
+    tags = models.ManyToManyField(Tag, blank=True)
+    
+    class Meta:
+        verbose_name = 'bookmark'
+        verbose_name_plural = 'bookmarks'
+        ordering = ['-date_created']
+        
+    def __str__(self):
+        return '%s (%s)' %(self.title, self.url)
